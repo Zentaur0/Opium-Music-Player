bfa46b40e5797736d5d26b6292b4535e88924d71
AntonSVTSV <freshbreathe@gmail.com>
2021-09-16 16:07:47 +0300

    view models added

---
+++ Opium Music Player/Sources/Models/ViewModels/SearchSectionViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  SearchSectionViewModel.swift
+//  Opium ~ Music Player for IPhone
+//
+//  Created by Антон Сивцов on 12.09.2021.
+//
+
+import Foundation
+
+// MARK: - SearchSectionViewModel
+
+struct SearchSectionViewModel {
+    let title: String
+    let results: [SearchResult]
+}
